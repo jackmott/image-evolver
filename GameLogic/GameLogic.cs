4000cd49586d9cb406ed43c60e80aac885d505e4
jack mott <jack.mott@gmail.com>
2018-12-21 12:49:34 -0600

    cleanup

--- GameLogic/GameLogic.cs
+++ GameLogic/GameLogic.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 using Microsoft.Xna.Framework.Content;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization;
 using System.Xml;
 
